fc19ed2a95aa87cefcc8236e917451e5a7a347b5
Alex <Lepiozdyx@gmail.com>
2025-01-10 09:51:45 -0300

    Initial Commit

---
+++ Foxwood/FoxwoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoxwoodApp.swift
+//  Foxwood
+//
+//  Created by Alex on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoxwoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
