352ce5056c05ff415937a155523501412892e442
Alex <Lepiozdyx@gmail.com>
2025-01-10 09:59:36 -0300

    Set up the poject, add assets

---
+++ Foxwood/App/FoxwoodApp.swift
@@ -0,0 +1,18 @@
+//
+//  FoxwoodApp.swift
+//  Foxwood
+//
+//  Created by Alex on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoxwoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
